cec9070cd416db6b77f9fea53cd2950cef8f8e2e
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-21 11:38:12 -0400

    Updated from Brython Server: 9/21/2018 11:38:11 AM

--- bullet.py
+++ bullet.py
@@ -6,12 +6,17 @@ noline = LineStyle(0, black)
 class ShooterTest(App):
     def __init__(self):
         super().__init__()
-        char=Char((0,0))
+        ShooterTest.listenMouseEvent('click', self.mouseClick)
+    
+    def mouseClick(self, event):
+        char=Char(event.x,event.y)
         
 class Char(Sprite):
     asset=RectangleAsset(15,15,noline, black)
-    def __init__(self, posititon):
+    def __init__(self, x,y):
         super().__init__(Char.asset)
+        self.x=x
+        self.y=y
 
 myapp=ShooterTest()
 myapp.run()
