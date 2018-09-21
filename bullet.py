b8505968d125d6a7b27f0929b9ebd79f867eac5c
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-21 11:33:00 -0400

    Updated from Brython Server: 9/21/2018 11:32:58 AM

--- bullet.py
+++ bullet.py
@@ -6,12 +6,12 @@ noline = LineStyle(0, black)
 class ShooterTest(App):
     def __init__(self):
         super().__init__()
-        char=Char()
+        char=Char((0,0))
         
 class Char(Sprite):
     asset=RectangleAsset(15,15,noline, black)
-    def __init__(self):
-        super().__init__()
+    def __init__(self, posititon):
+        super().__init__(Char.asset)
 
 myapp=ShooterTest()
 ShooterTest.run()
