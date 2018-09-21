fff62ddd31f1a3fc282ffeaf1941b2d3f698447a
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-21 11:31:12 -0400

    Updated from Brython Server: 9/21/2018 11:31:09 AM

--- bullet.py
+++ bullet.py
@@ -1,1 +1,17 @@
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+from ggame import App, RectangleAsset, CircleAsset, Sprite, LineStyle, Color
+
+black = Color(0, 1)
+noline = LineStyle(0, black)
+
+class ShooterTest(App):
+    def __init__(self):
+        super().__init__()
+        char=Char()
+        
+class Char(Sprite):
+    asset=RectangleAsset(15,15,noline, black)
+    def __init__(self):
+        super().__init__()
+
+myapp=ShooterTest()
+ShooterTest.run()
