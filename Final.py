e137c1d7508cd08ad4cd71f2de6ebd7051ed5e47
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-25 12:08:59 -0400

    Updated from Brython Server: 9/25/2018 12:08:58 PM

--- Final.py
+++ Final.py
@@ -23,7 +23,7 @@ class Member(Sprite):
         self.y=(self.y+self.targety+self.y)/3
         
 class Enemy(Sprite):
-    asset=ImageAsset("images/enemy_wheels.png")
+    asset=ImageAsset("images/enemy_wheels.png", Frame(0,0,159,133), 4, 'horizontal')
     def __init__(self):
         super().__init__(Enemy.asset)
         self.hp=200
