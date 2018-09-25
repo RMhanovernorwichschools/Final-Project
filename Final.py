01de560a20b9f6c710fc9f6afe35b6cb3dc83a09
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-25 07:51:31 -0400

    Updated from Brython Server: 9/25/2018 7:51:31 AM

--- Final.py
+++ Final.py
@@ -3,13 +3,8 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 class Game(App):
     def __init__(self):
         super().__init__()
-        m = ImageAsset("Map Base.jpg")
+        m = ImageAsset("Map Base.png")
         am=Sprite(m)
 
-myapp=App()
-
-m = ImageAsset("Map Base.jpg")
-am=Sprite(m)
-am.scale=5
-
+myapp=Game()
 myapp.run()
