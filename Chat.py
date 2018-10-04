ff8681d2275cff1f139e89090d63938b3482695f
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-10-04 08:43:39 -0400

    Updated from Brython Server: 10/4/2018 8:43:37 AM

--- Chat.py
+++ Chat.py
@@ -1,26 +1,48 @@
-string=input("Please enter a string of text (the bigger the better): ")
+string=list(input("Please enter a string of text (the bigger the better): "))
 sl=len(string)
 
 lowercase=list('abcdefghijklmnopqrstuvwxyz')
 uppercase=list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 characters=[]
+words=[]
 print_b=''
 
 for x in range(0,sl):
+    if string[x]!='.' and string[x]!=',' and string[x]!=':':
+        characters.append(string[x])
+    else:
+        sl-=1
+for x in range(0,sl):
     for n in range(len(uppercase)):
-        if x==uppercase[n]:
-            x=lowercase[n]
-    characters.append(string[x])
-
+        if characters[x]==uppercase[n]:
+            characters[x]=lowercase[n]
 
 space_loci=[]
 for x in range(0,sl):
     if characters[x]==' ':
         space_loci.append(x)
+        
+wordnum=len(space_loci)+1
+print(wordnum)
 
-reverse_space_loci=space_loci[len(space_loci):0:-1]
-reverse_space_loci.append(space_loci[0])
+wordnum_list=list(range(wordnum))
+
+for x in wordnum_list:
+    word=''
+    if x==0:
+        start=0
+        fin=space_loci[0]
+    elif x==len(wordnum_list)-1:
+        start=space_loci[x]
+        fin=sl-1
+    else:
+        start=space_loci[x]+1
+        fin=space_loci[x+1]
+    for n in range(start, fin):
+        word+=(characters[n])
+    words.append(word)
+    
 
 fin=sl
 for x in reverse_space_loci:
