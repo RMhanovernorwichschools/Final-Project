c0f7350952a9f94d1919612fcae446fc4dba8728
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-10-07 21:44:59 -0400

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -110,7 +110,7 @@ for x in range(6):
         if words[0]=='cool' or words[0]=='awesome' or words[0]=='great' or words[0]=='nice':
             agreement=['Probably.', 'Pretty {0}.'.format(words[0]), 'Fairly {0}.'.format(words[0]), 'Yeah, pretty {0}.'.format(words[0])]
             message=random.choice(agreement)
-        if words[0]=='how' or words[0]=='hows':
+        if words[0]=='how' or words[0]=='hows' and len(words)>2:
             mash=words[1]+words[2]
             if mash=='areyou' or mash=='areya' or mash=='ru' or mash=='areu':
                 message="I'm fine, thanks."
@@ -232,4 +232,3 @@ for x in range(6):
         
                 
 print(message)
-            
