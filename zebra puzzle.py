75b9458059832f9ff79a85b2fabf94d3d5545bf5
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-12-06 12:15:14 -0500

    Updated from Brython Server: 12/6/2018 12:15:12 PM

--- zebra puzzle.py
+++ zebra puzzle.py
@@ -1,8 +1,10 @@
-
+import random
 
 Names=['Willow', 'Lisbet', 'Cameron', 'Mariette', 'Annie']
 Scent=['pine', 'lavender', 'lemon', 'soap', 'fish']
 Shoes=['dinosaur boots', 'worn sneakers', 'winter boots', 'black rainboots', 'no shoes']
 BirthMons=['January', 'February', 'March', 'April', 'May']
 Coats=['hoodie', 'sweater', 'raincoat', 'down jacket', 'sports coat']
-Instruments=['piano', 'cello', 'violin', 'clarinet', '
+Colors=['navy blue', 'pale blue', 'white', 'grey', 'black']
+
+num_ob=input('How many items do you want the puzzle to contain? ')
