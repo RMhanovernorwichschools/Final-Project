6fdd4f5771e0c941e443e7e7dc169f2c713c14e7
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-12-06 12:13:45 -0500

    Update zebra puzzle.py

--- zebra puzzle.py
+++ zebra puzzle.py
@@ -1,1 +1,8 @@
 
+
+Names=['Willow', 'Lisbet', 'Cameron', 'Mariette', 'Annie']
+Scent=['pine', 'lavender', 'lemon', 'soap', 'fish']
+Shoes=['dinosaur boots', 'worn sneakers', 'winter boots', 'black rainboots', 'no shoes']
+BirthMons=['January', 'February', 'March', 'April', 'May']
+Coats=['hoodie', 'sweater', 'raincoat', 'down jacket', 'sports coat']
+Instruments=['piano', 'cello', 'violin', 'clarinet', '
