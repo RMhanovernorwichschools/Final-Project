b7bdc503ca313af5f4c7347627e4198ad3fc52c8
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-12-28 18:11:55 -0500

    Updated from Brython Server: 12/28/2018 6:11:54 PM

--- unrel.py
+++ unrel.py
@@ -41,19 +41,45 @@ for x in range(0,201):
             wait_time=(x/10)+aim
     dist+=vel
     if dist>=10:
-        state='analyze'
+        state='wait'
+        wait_time=(x/10)+aim
         vel=0
-    if state=='wait' and x>=wait_time:
+    if state=='wait' and x/10>=wait_time:
         state='analyze'
-    if x/10>e1_load:
+    
+    if x/10>=e1_load:
         e1_state='load'
         e1_wait=x/10+2
         e1_load=x/10+7
     elif e1_wait<=x/10:
         if e1_state=='load':
             e1_state='fire'
-        elif e1_state=='fire':
-            score-=5*((100*eva)/100)
             e1_wait+=0.5
-    print(score)
+        elif e1_state=='fire':
+            score-=5*((100-eva)/100)
+            e1_wait+=0.5
+    
+    if x/10>=e2_load:
+        e2_state='load'
+        e2_wait=x/10+2
+        e2_load=x/10+7
+    elif e2_wait<=x/10:
+        if e2_state=='load':
+            e2_state='fire'
+            e2_wait+=0.5
+        elif e2_state=='fire':
+            score-=5*((100-eva)/100)
+            e2_wait+=0.5
+            
+    if x/10>=e3_load:
+        e3_state='load'
+        e3_wait=x/10+2
+        e3_load=x/10+7
+    elif e3_wait<=x/10:
+        if e3_state=='load':
+            e3_state='fire'
+            e3_wait+=0.5
+        elif e3_state=='fire':
+            score-=5*((100-eva)/100)
+            e3_wait+=0.5
     
