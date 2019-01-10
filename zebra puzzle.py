b1ce67d9d0f9e6c3c5d07217c102b73fb9282a4f
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2019-01-10 14:47:09 -0500

    Updated from Brython Server: 1/10/2019 2:47:09 PM

--- zebra puzzle.py
+++ zebra puzzle.py
@@ -338,9 +338,14 @@ sort_clues(clues)
 for a in clues:
     print(a[4])
 
+r=0
 while (ready(gen_pos(),clues)) !=1:
+    r+=1
     generate_clue(random.choice(items).loci, random.choice(traits_fin), random.randint(1,3))
+    sort_clues(clues)
     print(ready(gen_pos(),clues))
+    if r=10:
+        break
 
 for a in clues:
     print(a[4])
